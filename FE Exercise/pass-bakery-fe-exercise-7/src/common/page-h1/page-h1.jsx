22c729214aaa6a25430f8f4eeecb8b54b6df3ad7
ngandrud98 <nicholas.gandrud@optum.com>
2022-11-17 14:54:53 -0600

    Created additional Jest Tests

---
+++ FE Exercise/pass-bakery-fe-exercise-7/src/common/page-h1/page-h1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PageH1 = styled.h1`
+  font-family: 'Qwitcher Grypen', serif;
+  font-size: 112px;
+`;
+
+export default PageH1;
